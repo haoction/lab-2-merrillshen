59d1e1d66dda39f774c38ed3170c76580a9dcbce
Merrill Shen <50651325+merrillshen@users.noreply.github.com>
2021-10-16 18:26:12 +0100

    Exercise 1 Task 1 Completion: Display the software counter value on the LED array in binary.

--- LEDarray.c
+++ LEDarray.c
@@ -6,9 +6,36 @@
 / Function used to initialise pins to drive the LEDarray
 ************************************/
 void LEDarray_init(void)
-{
-    	//set up TRIS registers for pins connected to LED array
-	//set initial output LAT values (they may have random values when powered on)
+{   
+    //set up TRIS registers for pins connected to LED array
+    //set initial output LAT values (they may have random values when powered on)
+    
+    LATGbits.LATG0=0;   //set initial output state
+    TRISGbits.TRISG0=0;
+    
+    LATGbits.LATG1=0;   //set initial output state
+    TRISGbits.TRISG1=0;
+
+    LATAbits.LATA2=0;   //set initial output state
+    TRISAbits.TRISA2=0;	
+	
+    LATFbits.LATF6=0;   //set initial output state
+    TRISFbits.TRISF6=0;
+    
+    LATAbits.LATA4=0;   //set initial output state
+    TRISAbits.TRISA4=0;	
+    
+    LATAbits.LATA5=0;   //set initial output state
+    TRISAbits.TRISA5=0;	
+    
+    LATFbits.LATF0=0;   //set initial output state
+    TRISFbits.TRISF0=0;
+    
+    LATBbits.LATB0=0;   //set initial output state
+    TRISBbits.TRISB0=0;
+    
+    LATBbits.LATB1=0;   //set initial output state
+    TRISBbits.TRISB1=0;
 }
 
 /************************************
@@ -20,6 +47,15 @@ void LEDarray_disp_bin(unsigned int number)
 	//some code to turn on/off the pins connected to the LED array
 	//if statements and bit masks can be used to determine if a particular pin should be on/off
 	//see Readme.md for examples
+    if (number & 0b00000000) { LATGbits.LATG0=1;} else { LATGbits.LATG0=0;}
+    if (number & 0b00000001) { LATGbits.LATG1=1;} else { LATGbits.LATG1=0;}
+    if (number & 0b00000010) { LATAbits.LATA2=1;} else { LATAbits.LATA2=0;}
+    if (number & 0b00000100) { LATFbits.LATF6=1;} else { LATFbits.LATF6=0;}
+    if (number & 0b00001000) { LATAbits.LATA4=1;} else { LATAbits.LATA4=0;}
+    if (number & 0b00010000) { LATAbits.LATA5=1;} else { LATAbits.LATA5=0;}
+    if (number & 0b00100000) { LATFbits.LATF0=1;} else { LATFbits.LATF0=0;}
+    if (number & 0b01000000) { LATBbits.LATB0=1;} else { LATBbits.LATB0=0;}
+    if (number & 0b10000000) { LATBbits.LATB1=1;} else { LATBbits.LATB1=0;}
 }
 
 /************************************
